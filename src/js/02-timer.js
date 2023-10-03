5afbd4ef9b5e17bf716667b2c5ddabed2f470c7a
Larsisko <arkadiusz.miazga1988@gmail.com>
2023-10-03 12:08:40 +0200

    counter error

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -11,6 +11,7 @@ const minutesElement = document.querySelector('[data-minutes]');
 const secondsElement = document.querySelector('[data-seconds]');
 const today = new Date();
 let selectedDate;
+startBtn.disabled = true;
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -22,7 +23,7 @@ const options = {
       window.alert('Please choose a date in the future');
       startBtn.disabled = true;
     } else {
-      selectedDate = selectedDates;
+      selectedDate = selectedDates[0];
       startBtn.disabled = false;
     }
   },
@@ -38,35 +39,17 @@ flatpickr(datetimePicker, options).config.onError = function (error) {
 function addLeadingZero(value) {
   return value.toString().padStart(2, '0');
 }
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6, minutes: 42, seconds: 20}
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6, minutes: 42, seconds: 20}
 
 let countdownInterval = null;
 
 startBtn.addEventListener('click', () => {
-  selectedDate = datetimePicker._selectedDates[0].getTime();
+  selectedDateMi = selectedDate.getTime();
 
-  let timeDifference = selectedDate - new Date().getTime();
+  let timeDifference = selectedDateMi - new Date().getTime();
 
   countdownInterval = setInterval(() => {
     const { days, hours, minutes, seconds } = convertMs(timeDifference);
@@ -85,3 +68,21 @@ startBtn.addEventListener('click', () => {
 
   startBtn.disabled = true;
 });
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
